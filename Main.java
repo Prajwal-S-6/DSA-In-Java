a4e9e45a81fb0f390775e7d155626a97edcc8182
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-03-25 19:49:01 +0530

    DSA Pattern problems in java

---
+++ Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(scanner.nextBoolean());
+
+//        InputStreamReader in  = new InputStreamReader(System.in);
+//
+//
+//        try(BufferedReader bf = new BufferedReader(in)) {
+//            var res = bf.readLine();
+//            System.out.println(Integer.parseInt(res));
+//        }  catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        Pattern.printPattern1(3);
+        Pattern.printPattern2(3);
+        Pattern.printPattern3(5);
+        Pattern.printPattern4(5);
+        Pattern.printPattern5(5);
+        Pattern.printPattern6(5);
+        Pattern.printPattern7(5);
+
+    }
+
+
+
+}
+
+
+
---
+++ Pattern.java
@@ -0,0 +1,133 @@
+public class Pattern {
+
+    /*
+
+     * * *
+     * * *
+     * * *
+
+     */
+    public static void printPattern1(int n) {
+        for(int i=0; i< n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("\n");
+        }
+    }
+
+
+    /*
+
+     *
+     * *
+     * * *
+
+     */
+    public static void printPattern2(int n) {
+        for(int i = n; i > 0; i--) {
+            for(int j = 0; j <= n-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+
+    /*
+
+        1
+        1 2
+        1 2 3
+
+     */
+    public static void printPattern3(int n) {
+        for(int i = n; i >= 0; i--) {
+            for(int j = 1; j <= n-i; j++) {
+                System.out.print(j +  " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+    /*
+        1
+        22
+        333
+        4444
+        55555
+     */
+
+    public static void printPattern4(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /*
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+     */
+    public static void printPattern5(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = n; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+    /*
+
+    1 2 3 4 5
+    1 2 3 4
+    1 2 3
+    1 2
+    1
+
+     */
+    public static void printPattern6(int n) {
+        for(int i = n; i > 0; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j +" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+    /*
+     *
+     ***
+     *****
+     *******
+     *********
+     */
+    public static void printPattern7(int n) {
+        int count = 1;
+        for(int i=1; i <= n; i++) {
+            for(int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            int counter = 0;
+            for(int k = 1; k <= count; k++) {
+                System.out.print("*");
+            }
+            count = count + 2;
+            for(int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
