f3e007bc78a488c89f542a2c1a54bf73423e0691
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-04-20 19:54:52 +0530

    array problems

--- com/dsa/arrays/Main.java
+++ com/dsa/arrays/Main.java
@@ -9,26 +9,20 @@ import static com.dsa.arrays.ArrayProblems.*;
 public class Main {
 
     public static void main(String[] args) {
-        // int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
-        // int[] arr = {5,4,-1,7,8};
-        // maximumSumSubArray(arr).stream().forEach(System.out::println);
+         int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
+         int[] arr1 = {5,4,-1,7,8};
+         maximumSumSubArray(arr1).stream().forEach(System.out::println);
 
-//        int[] arr = {-1, 1, 1};
-//        System.out.println(getLongestSubarray(arr, 10));
+        int[] arr2 = {-1, 1, 1};
+        System.out.println(getLongestSubarray(arr2, 10));
 
-//        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(6,7, 4, 2, 5, 3));
-//        findLeaders(list).forEach(System.out::println);
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(6,7, 4, 2, 5, 3));
+        findLeaders(list).forEach(System.out::println);
 
-//        int[] arr = {1,3,5,6};
-//        System.out.println(searchInsert(arr, 5));
 
-//        int[] arr = {3, 4, 4, 7, 8, 10};
-//        Arrays.stream(getFloorAndCeil(arr, 8)).forEach(System.out::println);
+        int[] arr3 = {2,1,3,4};
+        System.out.println(check(arr3));
 
-//        int[] arr = {5,7,7,8,8,10};
-//        searchRange(arr, 8);
 
-        int[] arr = {2,1,3,4};
-        System.out.println(check(arr));
     }
 }
