1b600c5d67111929efe8ebfd6fd0a61a6703b8e8
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-04-20 19:55:33 +0530

    binary search problems

---
+++ com/dsa/binarysearch/Main.java
@@ -0,0 +1,19 @@
+package com.dsa.binarysearch;
+
+import java.util.Arrays;
+
+import static com.dsa.binarysearch.BinarySearchProblems.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6};
+        System.out.println(searchInsert(arr, 5));
+
+        int[] arr1 = {3, 4, 4, 7, 8, 10};
+        Arrays.stream(getFloorAndCeil(arr, 8)).forEach(System.out::println);
+
+        int[] arr2 = {5,7,7,8,8,10};
+        searchRange(arr, 8);
+    }
+}
