4592697df9b182ced899d85951c3c2cd20903cb4
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-04-22 10:59:49 +0530

    BinarySearchProblems

--- com/dsa/binarysearch/BinarySearchProblems.java
+++ com/dsa/binarysearch/BinarySearchProblems.java
@@ -269,4 +269,33 @@ public class BinarySearchProblems {
         }
         return -1;
     }
+
+
+    // 162. Find Peak Element
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 1) {
+            return 0;
+        }
+        if(nums[0] > nums[1]) {
+            return 0;
+        } else if(nums[nums.length - 1] > nums[nums.length-2]) {
+            return nums.length-1;
+        }
+        int low = 1;
+        int high = nums.length - 2;
+
+        // intuition is that put a graph on the given elements, if mid comes on the increasing half then peak is on right side; else left side
+        while(low <= high) {
+            int mid = (low+high)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
+                return mid;
+            } else if(nums[mid] > nums[mid-1]) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+
+        return -1;
+    }
 }
