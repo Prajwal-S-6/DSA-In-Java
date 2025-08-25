f0a7baf082dc7783af258c188f0eff2a4abd3276
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-25 21:21:03 +0530

    BinarySearchProblems

--- com/dsa/binarysearch/Main.java
+++ com/dsa/binarysearch/Main.java
@@ -57,8 +57,20 @@ public class Main {
 
 //        System.out.println(maxDistanceBetweenGasStations(new int[] {1,2,3,4,5,6,7,8,9,10}, 1));
 
-        searchMatrix(new int[][] {{1},{3},{5}}, 1);
+        // searchMatrix(new int[][] {{1},{3},{5}}, 1);
 
+        //mySqrt(2147483647);
+
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(3);
+        treeNode.left.left = new TreeNode(4);
+        treeNode.left.right = new TreeNode(5);
+        treeNode.right.left = new TreeNode(6);
+
+        //countNodes(treeNode);
+
+        firstBadVersion(2126753390);
 
 
     }
