42357e9012897f218fb32d2dbd6f0d092e4568af
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-25 21:19:37 +0530

    BinarySearchProblems

--- com/dsa/binarysearch/BinarySearchProblems.java
+++ com/dsa/binarysearch/BinarySearchProblems.java
@@ -797,4 +797,19 @@ public class BinarySearchProblems {
         }
         return result;
     }
+
+
+
+     public static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+         TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
 }
